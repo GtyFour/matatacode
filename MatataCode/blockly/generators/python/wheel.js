e8fb6c7844605601a617054a44edd7fff0e01014
gtyfour <363268066@qq.com>
2019-09-09 09:56:29 +0800

    <Blockly-new blocks>新增轮子控制块

---
+++ MatataCode/blockly/generators/python/wheel.js
@@ -0,0 +1,41 @@
+'use strict';
+
+goog.provide('Blockly.Python.wheel');
+
+goog.require('Blockly.Python');
+
+
+// If any new block imports any library, add that library name here.
+Blockly.Python.addReservedWords('wheel');
+
+Blockly.Python['wheel_control'] = function(block) {
+  var dropdown_wheel = block.getFieldValue('WHEEL');
+  var dropdown_direction = block.getFieldValue('DIRECTION');
+  var dropdown_name = block.getFieldValue('NAME');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Python['both_wheel_control'] = function(block) {
+  var dropdown_direction = block.getFieldValue('DIRECTION');
+  var dropdown_speed = block.getFieldValue('SPEED');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Python['wheel_stop'] = function(block) {
+  var dropdown_wheel = block.getFieldValue('WHEEL');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Python['wheel_control_new'] = function(block) {
+  var dropdown_lspeed = block.getFieldValue('lspeed');
+  var dropdown_rspeed = block.getFieldValue('rspeed');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
